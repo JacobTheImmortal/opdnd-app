aa1476129e47281c38157501b284b859f84af703
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-07-25 13:34:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
-// Final App component with Supabase logic here
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
