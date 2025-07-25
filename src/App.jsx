ca264e507093ea9628ac6f32f91deb05c1ff8ef4
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-07-25 14:04:49 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>OPDND is Alive</h1>
-      <p>If you see this, the React app is working!</p>
+      <p>This is your app's main screen!</p>
     </div>
   );
 }
