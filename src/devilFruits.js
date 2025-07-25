471acdb0c04df3dccd268de0577739bce40c6dfa
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-07-25 19:40:39 -0400

    Create devilFruits.js

---
+++ src/devilFruits.js
@@ -0,0 +1,34 @@
+export const devilFruits = [
+  {
+    name: "Command Command Fruit",
+    ability: "Allows the user to issue verbal commands that force non-player characters or enemies to act or pause against their will. If the command is resisted, they lose 1 turn instead.",
+  },
+  {
+    name: "Vector Vector Fruit",
+    ability: "Allows the user to set a vector on a target they touch. The target is launched in the direction and distance of the vector set. Can be used on objects or people. Great for traversal or force launching.",
+  },
+  {
+    name: "Pavo Pavo Fruit",
+    ability: "Allows the user to generate explosions at points they designate with feathers. The feathers stick to enemies or surfaces. After being placed, the user can trigger the explosions at will.",
+  },
+  {
+    name: "Berry Berry Fruit",
+    ability: "Allows the user to manipulate, generate, or fire currency as a weapon. Sharp-edged coins can be launched with high velocity. Also allows absorption of currency into the body for mass and form.",
+  },
+  {
+    name: "Storm Storm Fruit",
+    ability: "Grants the user control over wind and storms. Can summon heavy gusts, lightning, and storm clouds. Also allows limited flight by riding currents of wind.",
+  },
+  {
+    name: "Zip Zip Fruit",
+    ability: "User can create zippers on any surface. Used to open portals, containers, or even split objects. Can create zipper-pockets to store items in space.",
+  },
+  {
+    name: "Color Color Fruit",
+    ability: "User can drain the color from an object or person. Colorless entities become intangible or frozen. The user can re-apply color to animate or restore them.",
+  },
+  {
+    name: "Lock Lock Fruit",
+    ability: "The user can 'lock' the position or state of an object. A locked door cannot be opened. A locked body cannot move. Locks last 1 turn unless broken by overwhelming force.",
+  }
+];
