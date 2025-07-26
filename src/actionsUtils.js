ef5e0217bb3b1fea8417abd3b44c1387b7602f10
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-07-26 19:42:10 -0400

    Create actionsUtils.js

---
+++ src/actionsUtils.js
@@ -0,0 +1,13 @@
+// actionsUtils.js
+
+export const defaultActions = [
+  { name: "Move", barCost: 0 },
+  { name: "Run", barCost: 0 },
+  { name: "Swim", barCost: 0 },
+  { name: "Dash", barCost: 0 },
+  { name: "Dodge", barCost: 10 },
+  { name: "Jump", barCost: 6 },
+  { name: "Equip", barCost: 2 },
+  { name: "Block", barCost: 5 },
+  { name: "Unarmed Strike", barCost: 5 }
+];
