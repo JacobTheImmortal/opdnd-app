151c92cc0362c3c8ed6429249a3c870c3fb2b67a
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-07-26 20:09:40 -0400

    Create equipmentData.js

---
+++ src/equipmentData.js
@@ -0,0 +1,8 @@
+export const equipmentList = [
+  { name: "Sword", damage: "10", durability: 15, value: 100 },
+  { name: "Gun", damage: "12", durability: 10, value: 200 },
+  { name: "Staff", damage: "5", durability: 20, value: 50 },
+  { name: "Axe", damage: "15", durability: 12, value: 150 },
+  { name: "Dagger", damage: "6", durability: 20, value: 60 },
+  { name: "Other", custom: true }
+];
