dc0673fa87af5f5620dc20b6dff1fb1b58e4cbf3
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-07-28 00:30:07 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://YOUR_PROJECT.supabase.co';
-const supabaseKey = 'your_anon_key';
+const supabaseUrl = https://sgwvigofvsmbenujnpar.supabase.co;
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnd3ZpZ29mdnNtYmVudWpucGFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NTc1NDksImV4cCI6MjA2OTAzMzU0OX0.M7aMKjfLlFGsgQlLRDl38IhlEStfP6EkGx9DKJWh7pQ;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
