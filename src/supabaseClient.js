46a0f402d4307f2b34e62554a904f610da2a2c7e
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-07-29 21:54:43 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sgwvigofvsmbenujnpar.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnd3ZpZ29mdnNtYmVudWpucGFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NTc1NDksImV4cCI6MjA2OTAzMzU0OX0.M7aMKjfLlFGsgQlLRDl38IhlEStfP6EkGx9DKJWh7pQ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
