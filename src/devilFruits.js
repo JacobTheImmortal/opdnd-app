3bc3262b05868692ecd5efc5cf7bfdb0f21ab235
JacobTheImmortal <jacobdavidkelly@gmail.com>
2025-08-17 23:32:52 -0400

    Update devilFruits.js

--- src/devilFruits.js
+++ src/devilFruits.js
@@ -1,71 +1,71 @@
 export const devilFruits = [
   {
     name: "Tired Tired",
-    ability: "Impose sleep and pervasive exhaustion; sap stamina from those nearby.",
+    ability: "Touch someone to induce Sleep(skip 1 turn). Aura of exhaust. Within your Melee Range, all beings lose 5 Bar At start of Turn while inside.",
   },
   {
     name: "Air Air",
-    ability: "Manipulate air to snuff flames, siphon air, generate gusts, and achieve brief flight.",
+    ability: "You are a being of air!!! You can control air around you up to a distance of your Base Movement. You take no damage from any physical non haki attacks. Your air control Includes Snuff - Remove air around a burning object to put it out. Siphon - Remove the air from a person to cause quicker exhaustion. Bar Cost is doubled for target. Flight - Move in all directions! Movement / Running Speed Used. PushBack - Targets in melee range get pushed back.",
   },
   {
     name: "Walk Walk",
-    ability: "Enhance motion and footing; interacts with terrain and momentum (utility-focused).",
+    ability: "You can walk on any Physical Surface. This means you cannot walk on Gases. Solids and Liquids work. Damaging materials still damage you.",
   },
   {
     name: "glycer glycer",
-    ability: "Volatile coating and detonation effects; crackling strikes and explosive finishes.",
+    ability: "You are Nitroglycerin! You can make and control Nitroglycerin. You take no damage from any physical non haki attacks. Abilities Include, Explode! You explode yourself and do damage to anyone in a 10ft radius. Crackle Fist, Your Melee attacks explode on contact for some extra damage. Coat, you can coat objects in your power. Next attack with that object will add explosion damage.",
   },
   {
     name: "Survive Survive",
-    ability: "Absorb ailments/afflictions and endure harsh conditions that break normal limits.",
+    ability: "You quickly become adept in different environments. You gain resistance to materials, weather, and poisons quickly. At low level you can gain 1 resistance at a time and 1 immunity. You can only hold 1 resistance and immunity. If poisoned, after 2 turns you gain immunity to that poison. If burning, you gain resistance to burning and will take half damage. Abilities include, Affliction absorption. You can take 1 Debuff from a target and put it on yourself.",
   },
   {
     name: "Twin Twin",
-    ability: "Create or shape duplicate bodies/constructs to act semi-independently.",
+    ability: "You make another you. You are now 2. You are still 1 but have two bodies. So far you can only make 1 body, and that Body seems to share your Bar. This other body has its own health though. It's half of your max health. The body is naked when you make it. Can't make clothes or weapons. Just you.",
   },
   {
     name: "Water Water",
-    ability: "Shape, separate, and project water; manipulate moisture content of targets.",
+    ability: "You seem to be able to do cool stuff with the water around you. Mainly you're able to separate the Sea water into Fresh water and other particulates. This is not a Logia. You can control sources of water around you Abilities Include Water Ball. Take a source of water and whip it at a target. Shape Water, Take a source of water and shape it into whatever you desire. Separate, Take the water out of a liquid. Moisture Grab, Take the water out of the air.",
   },
   {
     name: "Sound Sound",
-    ability: "Control sound: amplify, mute, silence, or alter auditory effects in an area.",
+    ability: "You are a sound man. You can make sounds, remove sounds, multiply sounds. Abilities include Auditory effect. Make a sound no louder than you can yell. Any sound you want. Silence, Remove a sound within a 60ft radius. Amp Up, Increase the volume of a sound for damaging effects within a 60ft radius.",
   },
   {
     name: "Dance Dance",
-    ability: "Compel rhythm and motion; set the stage for disco-fueled effects.",
+    ability: "You are a boogeyman. You can create a disco ball on any surface which plays music. Any music you want actually. Anyone who hears it within a 60ft radius wants to boogie down. Targets Under the dance spell will have to roll a Con saving throw in order to be able to do anything on their turn. If they succeed they still lose 1 Action as rolling the saving throw will count as an Action. You can't pick and choose who is affected by the rhythmic spell. You are not affected.",
   },
   {
     name: "Race Race",
-    ability: "Temporarily assume traits of other races for adaptive capabilities.",
+    ability: "You can change your form to look like any race. You can also touch a person to make them look like another race.",
   },
   {
     name: "Bear Bear Model Panda",
-    ability: "Zoan: transform between humanlike, half‑panda, and full panda forms.",
+    ability: "You can activate different Bear forms as a panda. HumanLike, Half Panda, Full Panda.  Human like means your in your standard form. No special effects apply, but it wont drain your bar each turn. Half Panda Drains 5 Bar each turn you have it up. Half Panda gives you claws that provide an extra damage boost to all unarmed melee attacks. Kicking/Punching. You also get advantage on rolls for dodging. Full Panda gives you a natural Block. You can use Block during a turn without the Bar cost. You get Disadvantage on rolls for dodging.",
   },
   {
     name: "State State",
-    ability: "Switch personal state/stance to unlock different capabilities.",
+    ability: "You can change an object's state of being. A metal object can become a rubbery one. A wooden object can become as hard as stone! A liquid can be a solid, A solid can be gelatinous. You don't seem to be able to turn materials into gaseous forms. You also can't make Solid materials completely liquid. There seems to be some requirement of structure. You tried to turn a gun into lava, but it didn't work. You just got a hot gun. Burned your hand since you were holding onto it. You can't change the form and temperature of an object at the same time. Also once you change the state of an object 1 way, you can't change it anymore. Just back to normal. You are able to change 1 object’s state at a time.",
   },
   {
     name: "Sit Sit",
-    ability: "Manipulate position and gravity wells; set, push, or pull objects and foes.",
+    ability: "Simply, you can sit anywhere. On a cloud, on the sea, on a moving cannon ball. You can sit on hot things, cold things, big things, and old things. Sit on a bear, someone’s hair, or even the air. Whatever you desire, you can sit anywhere!",
   },
   {
     name: "Cellar cellar",
-    ability: "Bottle and store essences or entities; release them at will.",
+    ability: "You can create a cellar door on a solid surface. One that you're able to easily stand on. You can make it so that anyone can open it or just you. Once you enter it, you're in your own pocket dimension. It's an empty cellar. Put whatever you want into it.Time moves the same in the cellar as outside the cellar. The cellar Can only be locked from the outside unless you are also inside the cellar. If a person(including you) is in the cellar, the door will not disappear. If the ground where the cellar resides is destroyed, the door will instantly move to the nearest possible location. You lose no Bar in the cellar. Any items destroyed in the cellar will be instantly repaired upon a cellar reset. You can only reset it outside.",
   },
   {
     name: "Rage rage",
-    ability: "Stoke feral fury in yourself or others; escalate aggression and power.",
+    ability: "You can cause Rage upon a target. The Target will gain double damage on any melee attack. The target will also gain an extra action point for an attack roll. The target will have to use all action points. The target loses all concentration and will only be able to attack the closest active being to it. The Target’s first action must be an attack. You can only rage 1 target at a time. You cannot rage yourself as the power requires you to concentrate. Once the target gets to 0 Bar, the rage will instantly wear off.",
   },
   {
     name: "Potion potion",
-    ability: "Brew quick infusions from minor to potent elixirs for varied effects.",
+    ability: "You are pretty cool, or warm, or fast, or sleepy, or high. Whatever you just drank probably did it. You can take up to 8 oz of any liquid and add effects to it. Once consumed, the effects apply. Effects include -  Positive - (Strength, Reflexive, Energize, Revitalize, Aware, Tough). Negative - (Weak, Hazed, Exhaust, Drunk, Explode, PaperThin). Your potions don't work when thrown. They only take effect once a target drinks a full 8 oz of the material. Potions last 1 hour or until used up. You can add up to 2 Positive effects to any potion, but you have to also add 1 negative effect for every 2 positive ones. Negative effects will take place after positive ones wear off.",
   },
   {
     name: "Rat rat",
-    ability: "Command vermin swarms and crown a Rat King to overwhelm foes.",
+    ability: "You are a rat man. You are unaffected by poisons and diseases. Debuffs in general. But only 3 at a time. A long rest will filter out any afflictions you're currently carrying. Rats also seem to like you. You can't talk to them, but rats will pop up. You have rat eyes. So you got dark vision.",
   },
   {
     name: "Phobia phobia",
